4d171f7e51dd6825cd76bc0d8c3c66b617cf35c3
Michael Garbuz <garbuzmichael@gmail.com>
2019-04-19 14:33:04 +1000

    added multiple files for the html server side view as well as input boxes, working on API requests from database now. Database setup with MongoDB

--- source/app.js
+++ source/app.js
@@ -1,1 +1,54 @@
 const express = require('express')
+const hbs = require('hbs')
+const path = require('path')
+
+console.log(__dirname)
+console.log(path.join(__dirname,'../public'))
+
+const app = express()
+const port = 3000
+// define paths for express coniguration
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+// setup handle bars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+// setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req,res) => {
+    res.render('login', {
+        title: 'Login',
+        name: 'Michael Garbuz'
+    })
+})
+
+app.get('/store',(req,res) => {
+    res.render('store', {
+        title: 'Online Movie Store',
+        name: 'Michael Garbuz'
+    })
+})
+
+app.get('/about',(req,res) => {
+    res.render('about', {
+        title: 'About Us',
+        name: 'Michael Garbuz'
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help', {
+        title: 'Help Page',
+        name: 'Michael Garbuz',
+        helpText: 'Insert help text'
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server up on port ' + port)
+})
