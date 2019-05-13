99a5e4788124859479f718bae05313f39512b916
Michael Garbuz <garbuzmichael@gmail.com>
2019-05-13 18:48:06 +1000

    fixed up icons and hover on side bar

--- public/js/clientside.js
+++ public/js/clientside.js
@@ -4,6 +4,25 @@ const loginForm = document.querySelector('form')
 const username =  document.querySelector('#userName')
 const password =  document.querySelector('#password')
 
+$(document).ready(function() {
+	var source = $("nav-img").attr('src');
+	console.log(source);
+	var notHover = String(source).includes("hover");
+	if (notHover == true) {
+		return; }
+	else {
+			$(".nav-img").hover(function(){
+				$(this).attr("src", function(index, attr){
+					return attr.replace(".png", "-hover.png").replace("Base_Side_Bar", "Hover_Side_Bar");
+				});
+				}, function(){
+				$(this).attr("src", function(index, attr){
+					return attr.replace("-hover.png", ".png").replace("Hover_Side_Bar", "Base_Side_Bar");
+				});
+			});
+		 }
+	});
+
 loginForm.addEventListener('submit',(e) => {
     e.preventDefault()
     const enteredUsername = username.value
